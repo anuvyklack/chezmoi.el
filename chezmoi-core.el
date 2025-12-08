c1fb4737914531ff08830da03ca14e2562096778
Yuriy Artemyev <anuvyklack@gmail.com>
2025-12-08 03:27:12 +0300

    Docs

--- chezmoi-core.el
+++ chezmoi-core.el
@@ -1,31 +1,29 @@
-;;; chezmoi-core.el --- A package for interacting with chezmoi -*- lexical-binding: t -*-
+;;; chezmoi-core.el --- Interact with chezmoi from Emacs -*- lexical-binding: t -*-
 
 ;; Author: Harrison Pielke-Lombardo
-;; Maintainer: Harrison Pielke-Lombardo
-;; Version: 1.1.0
-;; Package-Requires: ((emacs "26.1"))
-;; Homepage: http://www.github.com/tuh8888/chezmoi.el
-;; Keywords: vc
+;; Maintainer: Yuriy Artemyev
+;; Version: 2.0.0
+;; Package-Requires: ((emacs "28.1"))
+;; Homepage: http://www.github.com/anuvyklack/chezmoi.el
 
 
 ;; This file is not part of GNU Emacs
-
+;;
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; For a full copy of the GNU General Public License
 ;; see <http://www.gnu.org/licenses/>.
 
-
 ;;; Commentary:
-
+;;
 ;; Chezmoi is a dotfile management system that uses a source-target state
 ;; architecture.  This package provides convenience functions for maintaining
 ;; synchronization between the source and target states when making changes to
@@ -33,7 +31,7 @@
 ;; `save-buffer' for source state files which maintain synchronization to the
 ;; target state.  It also provides diff/ediff tools for resolving when dotfiles
 ;; get out of sync.  Dired and magit integration is also provided.
-
+;;
 ;;; Code:
 
 (defgroup chezmoi nil
--- extensions/chezmoi-dired.el
+++ extensions/chezmoi-dired.el
@@ -1,33 +1,31 @@
 ;;; chezmoi-dired.el --- Dired integration for chezmoi -*- lexical-binding: t -*-
 
 ;; Author: Harrison Pielke-Lombardo
-;; Maintainer: Harrison Pielke-Lombardo
-;; Version: 1.1.0
-;; Package-Requires: ((emacs "26.1") (chezmoi "1.1.0"))
-;; Homepage: http://www.github.com/tuh8888/chezmoi.el
-;; Keywords: vc
+;; Maintainer: Yuriy Artemyev
+;; Version: 2.0.0
+;; Package-Requires: ((emacs "28.1"))
+;; Homepage: http://www.github.com/anuvyklack/chezmoi.el
 
 
 ;; This file is not part of GNU Emacs
-
+;;
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; For a full copy of the GNU General Public License
 ;; see <http://www.gnu.org/licenses/>.
 
-
 ;;; Commentary:
-
+;;
 ;; Provides `dired' integration for `chezmoi'.
-
+;;
 ;;; Code:
 
 (require 'dired)
